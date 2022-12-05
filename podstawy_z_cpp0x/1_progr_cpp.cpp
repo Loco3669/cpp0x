e2faede3911a9779683d917705b59b801eb8a71f
Christian <krystian_biskup@interia.pl>
2022-12-05 21:20:57 +0100

    first commit

---
+++ podstawy_z_cpp0x/1_progr_cpp.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <cstdio>
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+    printf ("\t To Ja \nLOL xD");
+}//test
---
+++ podstawy_z_cpp0x/2_zabawa_cout_printf.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+int main()
+{
+    std::cout << "==============" << std::endl;
+    std::cout << "= To proste! =" << std::endl;
+    std::cout << "==============" << std::endl;
+    std::cout << "    A jak to zrobic?" << std::endl;
+    std::cout << "Koniec :)" << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+    printf("==============\n");
+    printf ("= To proste! =\n");
+    printf ("==============\n");
+    printf ("\tA jak to zrobic?\nKoniec :)");
+}
