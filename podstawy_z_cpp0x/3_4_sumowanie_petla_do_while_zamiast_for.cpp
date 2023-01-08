b8b2ae3409641050f713ca5ad8d2380ad2f346e8
Christian <krystian_biskup@interia.pl>
2023-01-08 14:49:37 +0100

    Napisanie funkcji sumowania przy pomocy do while zamiast for

---
+++ podstawy_z_cpp0x/3_4_sumowanie_petla_do_while_zamiast_for.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+void wypiszTablice( int tablica[], int ile )
+{
+    for( int indeks = 0; indeks < ile; indeks++ )
+         std::cout << tablica[ indeks ] << std::endl;
+   
+}
+
+int sumujLiczby( int tablica[], int ile )
+{
+    int oblicz = 0;
+    int indeks = 0;
+    if (indeks < ile){
+        do {
+            oblicz += tablica[ indeks ];
+            indeks++;
+        } while (indeks < ile);
+    }
+   
+    return oblicz;
+}
+
+int main()
+{
+    int dane[ 5 ];
+    dane[ 0 ] = 11;
+    dane[ 1 ] = 5;
+    dane[ 2 ] = 7;
+    dane[ 3 ] = 8;
+    dane[ 4 ] = 2;
+    wypiszTablice( dane, 5 );
+    int wynik = sumujLiczby( dane, 5 );
+    std::cout << "Suma liczb wynosi = " << wynik << std::endl;
+    return 0;
+}
