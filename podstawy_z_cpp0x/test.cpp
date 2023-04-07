ab44af24154bd8d73ee80ae0afbaed6b29cce5a5
Christian <krystian_biskup@interia.pl>
2023-04-07 21:52:32 +0200

    Sprawdzenie commitów

---
+++ podstawy_z_cpp0x/test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(){
+
+    long long wynik = 0;
+    for ( int a = 1; a <= 44; a++ ){
+        for ( int b = a + 1; b <= 45; b++ ){
+            for ( int c = b + 1; c <= 46; c++ ){
+                for ( int d = c + 1; d <= 47; d++ ){
+                    for ( int e = d + 1; e <= 48; e++ ){
+                        for ( int f = e + 1; f <= 49; f++ ){
+                            wynik++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    return 0;
+
+}
