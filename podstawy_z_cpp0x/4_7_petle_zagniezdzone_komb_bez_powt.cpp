16a7d05e09cf2e4d84968082bb43b2f5ddb85d68
Christian <krystian_biskup@interia.pl>
2023-04-07 16:43:36 +0200

    Wyobraź sobie, że masz pięć piłek i chcesz wylosować trzy z nich. Napisz program, który wypisze na ekranie wszystkie możliwe kombinacje piłek jakie można wylosować. Dane jakie powinny zostać wypisywane na ekranie.

--- podstawy_z_cpp0x/4_7_petle_zagniezdzone_komb_bez_powt.cpp
+++ podstawy_z_cpp0x/4_7_petle_zagniezdzone_komb_bez_powt.cpp
@@ -5,14 +5,13 @@ int main(){
     for ( int a = 1; a <= 3; a++ ){
         for ( int b = a + 1; b <= 4; b++ ){
             for ( int c = b + 1; c <= 5; c++ ){
-                for ( int d = c + 1; d <= 5; d++ ){
+                
                     std::cout << a << " ";
                     std::cout << b << " ";
                     std::cout << c << " ";
-                    std::cout << d << " ";
                     std::cout << std::endl;
                 
-                }
+                
             }
         }
     }
