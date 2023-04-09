8adad7524083b6287b204458d8e7fb64aaffb262
Christian <krystian_biskup@interia.pl>
2023-04-09 13:49:55 +0200

    Napisz program, który odczyta zawartość pliku, a następnie wypisze na ekranie tylko te wiersze, w których znajduje się wyraz wprowadzony przez użytkownika.

---
+++ podstawy_z_cpp0x/5_0_podstawy_obslugi_plikow.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+bool wyswietlZawartoscPliku( std::string sNazwaPliku, std::string SzukanaFraza )
+{
+    std::ifstream plik;
+    plik.open( sNazwaPliku.c_str() );
+    if( !plik.good() )
+         return false;
+   
+    std::string wiersz;
+    int i = 1;
+   
+    while( std::getline( plik, wiersz ) )
+    {
+        size_t znalezionaPozycja = wiersz.find( SzukanaFraza );
+        if( znalezionaPozycja == std::string::npos )
+             std::cout << i << ". Brak" << std::endl;
+        else
+             std::cout << i << ". " << wiersz << std::endl;
+       
+        i++;
+       
+    }
+   
+    plik.close();
+    return true;
+}
+
+
+int main()
+{
+    std::string SzukanaFraza;
+    std::cout << "Wprowadz fraze do wyszukania w pliku: ";
+    std::cin >> SzukanaFraza;
+    if( !wyswietlZawartoscPliku( "cpp0x.txt", SzukanaFraza ) )
+         std::cout << "Nie udalo sie otworzyc pliku o podanej nazwie." << std::endl;
+   
+    return 0;
+}
